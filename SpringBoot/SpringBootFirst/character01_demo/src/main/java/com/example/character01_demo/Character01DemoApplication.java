a20fab7f7256da5319639f58b6fcf8b342d58e75
wang_idea <1025749823@qq.com>
2019-06-26 18:21:05 +0800

    firsttest

---
+++ SpringBoot/SpringBootFirst/character01_demo/src/main/java/com/example/character01_demo/Character01DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.character01_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Character01DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Character01DemoApplication.class, args);
+    }
+
+}
